ed371b96626213e2dde7b827f842789b088e2d6a
TeodoroDavi <daviteodoroinfluence12@gmail.com>
2025-05-23 18:11:38 -0300

    first commit

---
+++ MeuAppDeNavegacao/src/screens/HomeScreen.js
@@ -0,0 +1,43 @@
+import React from 'react'; 
+import {View, Text,Button,StyleSheet,Dimensions} from 'react-native';
+
+const windowWidth = Dimensions.get('window').width;
+
+export default function HomeScreen({navigation}){
+    return(
+        <View style = {styles.container}>
+            <Text style={styles.title}>Home Screen</Text>
+            <View style={styles.buttonContainer}>
+                <Button
+                  title="Go to Details"
+                  onPress={()=> navigation.navigate('Details')}
+              />
+            </View>
+            <View style={styles.buttonContainer}>
+                <Button 
+                  title ="Go to Profile"
+                  onPress={()=> navigation.navigate('Profile')}
+                />
+            </View>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    container : {
+        flex:1,
+        justifyContent:'center',
+        alignItems:'center',
+        backgroundColor:'#f0f8ff', //cor do fundo da tela
+    },
+    title: {
+        fontSize:24,
+        marginBottom:20,
+    },
+    buttonContainer:{
+        backgroundColor:'#add8e6',//cor de fundo fo container do botão
+        margin:10,
+        width: windowWidth * 0.5, // 50% da largura da tela 
+        borderRadius: 5,
+    },
+});
